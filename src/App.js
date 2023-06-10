588868c43183c62e4e8a837535d4f29bb0be1e48
kurrushadrak <kurrushdrk1@gmail.com>
2023-06-10 13:16:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const StartButton=()=>{
+
+  }
+  const RestartButton=()=>{
+
+  }
+  const StopButton=()=>{
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="stopwatch">
+      <h1>Stopwatch</h1>
+      <div className="timer">
+        <img src="https://assets.ccbp.in/frontend/react-js/stopwatch-timer.png "style={{marginRight:5}}/><span>Timer</span>
+        <h1>00:00</h1>
+        <button className='btn btn-primary' style={{marginRight:10}} onClick={StartButton}>Start</button>
+        <button className='btn btn-warning' style={{marginRight:10}} onClick={RestartButton}>Restart</button>
+        <button className='btn btn-danger' style={{marginRight:10}} onClick={StopButton}>Stop</button>
+      </div>
+     </div>
+       
     </div>
   );
 }
